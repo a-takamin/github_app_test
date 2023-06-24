4ee0d5d93d90e701f24b8ebce1647cccf02fe6c9
aki takami <aki-takami@cybozu.co.jp>
2023-06-25 01:02:37 +0900

    get installation token

--- checkrun-test/src/index.mjs
+++ checkrun-test/src/index.mjs
@@ -1,24 +1,32 @@
 import crypto from "crypto"
-import fs from "fs"
 import jwt from "jsonwebtoken"
+import https from "https"
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm"
 
 export const handler = async (event, context) => {
   const webhook_secret = await getSSMParameter(process.env.WEBHOOK_SECRET)
   const github_app_id = await getSSMParameter(process.env.GITHUB_APP_ID)
-  const github_app_pem = await getSSMParameter(process.env.GITHUB_APP_PRIVATE_KEY)
+  const github_app_private_key = await getSSMParameter(process.env.GITHUB_APP_PRIVATE_KEY)
 
   // まずは、GitHub App Webhookの認証を行う
   const signature = event.headers["x-hub-signature-256"]
-  // verify_signature(signature, JSON.stringify(event.body), webhook_secret)
   verify_signature(signature, event.body, webhook_secret)
 
-  // 次に、installation tokenの発行を行う
+  // 次に、installation access tokenの発行を行う
+  const installation_id = JSON.parse(event.body).installation.id
   const jwtoken = await create_jwt(github_app_id, github_app_private_key)
-  const installation_id = event.body.installation.id
+
+  let resp = ""
+  try { // .then().catch()ではなくawaitを使おうと思うので、try-catchでエラーをキャッチする
+    resp = await get_installation_access_token(installation_id, jwtoken)
+  } catch (e) {
+    console.error(e)
+  }
+  const inst_access_token = JSON.parse(resp).token
 
   // 最後に、GitHub APIを叩いて、Check Runsを作成する
-  return ""
+
+
 }
 
 const verify_signature = (signature, body, secret) => {
@@ -26,9 +34,6 @@ const verify_signature = (signature, body, secret) => {
     .update(body)
     .digest("hex")
   const checksum = `sha256=${digest}`
-  console.log("body", body)
-  console.log("checksum", checksum)
-  console.log("signature", signature)
   if (checksum !== signature) {
     throw new Error("signature does not match")
   }
@@ -42,6 +47,40 @@ const create_jwt = async (app_id, private_key) => {
   }
   // GitHub側が公開鍵を持っているので、秘密鍵で署名する
   return await jwt.sign(payload, private_key, { algorithm: "RS256" }) 
+}
+
+const get_installation_access_token = (installation_id, jwtoken) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "api.github.com",
+      port: 443,
+      path: `/app/installations/${installation_id}/access_tokens`,
+      method: "POST",
+      headers: {
+        "Accept": "application/vnd.github+json",
+        "User-Agent": "checkrun-test", // 必須。https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required
+        "X-Github-Api-Version": "2022-11-28",
+        "Authorization": `Bearer ${jwtoken}`,
+        "installation_id": installation_id,
+      }
+    }
+  
+    const req = https.request(options, (res) => {
+      let body = ""
+      res.on("data", (d) => {
+        body += d
+      })
+      res.on("error", (e) => {
+        reject(e)
+      })
+      res.on("end", () => {
+        resolve(body) // コールバック内のここでPromiseが解決される
+      })
+    })
+  
+    req.end() // ここでリクエストが送信される
+  })
+  
 }
 
 const getSSMParameter = async (path) => {
